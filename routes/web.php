80b20a71ae1b60989fcc6195945068a05b2fd560
eduardo <edulon2015@gmail.com>
2022-05-16 17:38:49 -0300

    Vendor

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/index.php', function () {
     return view('welcome');
 });
 Route::get('/cadastro',function(){
